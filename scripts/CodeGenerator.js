6fdf27ea1a0b2e0351a463c3e31f818a7ec91040
Mario Balić <mario.balic1@gmail.com>
2020-10-29 17:12:53 +0100

    Update CodeGenerator.js

--- scripts/CodeGenerator.js
+++ scripts/CodeGenerator.js
@@ -1,4 +1,4 @@
-/* var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
 if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
 {
   let window = _____WB$wombat$assign$function_____("window");
@@ -9,7 +9,7 @@ if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; re
   let parent = _____WB$wombat$assign$function_____("parent");
   let frames = _____WB$wombat$assign$function_____("frames");
   let opener = _____WB$wombat$assign$function_____("opener");
-*/
+
 var applicationKey = "881e674f-7891-4c20-afd8-56fe2624c4b5";
 var secretKey = "YCFd6hiA8lUjZejVcIf/LhRXO4wTDxY0JhOXvQZwnMSiNynSxmNIMjMf1HHwdV6cMN48NX3ZipA9q9hLPb9C1ZIzMH5dvELPAHceiu7LbZzmIAGeOf/OUaDrk2Zq2dbGacIAzU6yyk4KmOXRaSLi8KW8t3krdQSX7Ecm8Qunc/A=";
 var apiUrl = "https://test.netgiro.is/api/";
@@ -53,4 +53,4 @@ function GetRequest(e, signature, url, nonce)
 
 
 
-//}
+}
